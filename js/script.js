9f337b61520d5d1c3e03a85c00bd0f122e489be8
davidecaputo <daca9621@gmail.com>
2023-03-10 13:10:31 +0100

    add rules css and js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 const boxKm = document.querySelector('input[name="km"]');
 const boxAnni = document.querySelector('input[name="età"]');
 const button = document.querySelector('button');
+const calcolo = document.querySelector('.calcola');
+const cancella = document.querySelector('.reset');
 const biglietto = document.getElementById('biglietto');
+const acquista = document.querySelector('.box-compra');
+const compra = document.querySelector('.compra');
+const contenitore = document.querySelector('.content');
+console.log(acquista);
 console.log(boxKm);
 console.log(boxAnni);
 console.log(button);
 console.log(biglietto);
 
-button.addEventListener('click', function () {
+compra.addEventListener('click', function() {
+    acquista.classList.add('d-none');
+    contenitore.classList.remove('d-none');
+})
+
+calcolo.addEventListener('click', function() {
     let km = boxKm.value;
     let anni = boxAnni.value;
     let prezzoBiglietto = km * 0.21;
@@ -19,13 +30,26 @@ button.addEventListener('click', function () {
 
     if(km && anni){
         if(anni <= 18){
-            biglietto.innerHTML = `<h2>${scontoMinorenni.toFixed(2) + '€'}</h2>`
+            biglietto.classList.remove('text-danger');
+            biglietto.classList.add('text-primary');
+            biglietto.innerHTML = `<h1>Costo biglietto:</h1> <h2>${scontoMinorenni.toFixed(2) + '€'}</h2>`
         } else if(anni > 65) {
-            biglietto.innerHTML = `<h2>${scontoOver65.toFixed(2) + '€'}</h2>`
+            biglietto.classList.remove('text-danger');
+            biglietto.classList.add('text-primary');
+            biglietto.innerHTML = `<h1>Costo biglietto:</h1> <h2>${scontoOver65.toFixed(2) + '€'}</h2>`
         } else {
-            biglietto.innerHTML = `<h2>${prezzoBiglietto.toFixed(2) + '€'}</h2>`
+            biglietto.classList.remove('text-danger');
+            biglietto.classList.add('text-primary');
+            biglietto.innerHTML = `<h1>Costo biglietto:</h1> <h2>${prezzoBiglietto.toFixed(2) + '€'}</h2>`
         }
     } else {
+        biglietto.classList.add('text-danger');
         biglietto.innerHTML = `<h2>Devi inserire dei valori</h2>`
     }
+});
+
+cancella.addEventListener('click', function() {
+    boxKm.value = '';
+    boxAnni.value = '';
+    biglietto.innerHTML = '';
 });
